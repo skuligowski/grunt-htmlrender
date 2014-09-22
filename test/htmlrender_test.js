689739afbd9df5d66b838367ad14236af85d825d
Sebastian Kuligowski <sebastian@kuligowski.pl>
2014-09-22 22:22:05 +0200

    Tests for task

--- test/htmlrender_test.js
+++ test/htmlrender_test.js
@@ -37,7 +37,7 @@ exports.htmlrender = {
     test.done();
   },
   custom_options: function(test) {
-    test.expect(1);
+    test.expect(1);;
 
     var actual = grunt.file.read('tmp/custom_options');
     var expected = grunt.file.read('test/expected/custom_options');
